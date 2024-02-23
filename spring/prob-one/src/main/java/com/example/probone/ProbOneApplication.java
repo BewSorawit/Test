a6502f7e82f27c43899ceb59ff0ef3c24316068d
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-23 23:46:27 +0700

    init

---
+++ spring/prob-one/src/main/java/com/example/probone/ProbOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.probone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbOneApplication.class, args);
+	}
+
+}
---
+++ spring/prob-one/src/test/java/com/example/probone/ProbOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.probone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
